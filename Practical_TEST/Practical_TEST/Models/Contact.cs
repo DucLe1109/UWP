76d16dee0113dc63d40cefe9a34f6edc8c811b36
DucLe1109 <74303018+DucLe1109@users.noreply.github.com>
2021-03-19 20:00:01 +0700

    final
    1212

---
+++ Practical_TEST/Practical_TEST/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_TEST.Models
+{
+    public class Contact
+    {
+        public string name { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
---
+++ Practical_TEST/Practical_TEST/Models/DataAccess.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using Windows.Storage;
+using System.IO;
+using System.Data;
+
+namespace Practical_TEST.Models
+{
+    public class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync("practical_TEST.db", CreationCollisionOption.OpenIfExists);
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "practical_TEST.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Contact (Primary_Key INTEGER PRIMARY KEY, " +
+                    "name text NULL," +
+                    "phoneNumber text NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+        public static void AddData(string name,string phoneNumber)
+        {
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "practical_TEST.db");
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO Contact VALUES (NULL, @name,@phoneNumber);";
+                insertCommand.Parameters.AddWithValue("@name", name);
+                insertCommand.Parameters.AddWithValue("@phoneNumber", phoneNumber);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+        public static List<Contact> GetData(String name_search)
+        {
+            string abc = name_search;
+            List<Contact> entries = new List<Contact>();
+
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "practical_TEST.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * from Contact" + "where name = @name_search", db);
+                selectCommand.Parameters.Add("@name_search", (SqliteType)SqlDbType.Text);
+                selectCommand.Parameters["@name_search"].Value = name_search;
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(new Contact { 
+                    name = query.GetString(1),
+                    phoneNumber = query.GetString(2)
+                    });
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
